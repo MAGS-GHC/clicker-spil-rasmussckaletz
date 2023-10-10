29fb537a5e30c1fe54b49e3836344033c63a73b3
rasmussckaletz <rasmus@leeloo.dk>
2023-10-10 21:52:27 +0200

    Autoclicker implementeret

--- app.js
+++ app.js
@@ -25,33 +25,6 @@ const multiplierCookiePrice = {
     level10Price: 100000000,
 }
 
-const autoclickerLevel = {
-    level0: 1,
-    level1: 2,
-    level2: 3,
-    level3: 4,
-    level4: 5,
-    level5: 6,
-    level6: 7,
-    level7: 8,
-    level8: 9,
-    level9: 10,
-    level10: 10,
-}
-
-const autoclickerTime = {
-    level1Time: 10000,
-    level2Time: 9000,
-    level3Time: 8000,
-    level4Time: 7000,
-    level5Time: 6000,
-    level6Time: 5000,
-    level7Time: 4000,
-    level8Time: 3000,
-    level9Time: 2000,
-    level10Time: 1000,
-}
-
 const autoclickerPrice = {
     level1Price: 10,
     level2Price: 100,
@@ -65,9 +38,22 @@ const autoclickerPrice = {
     level10Price: 1000000000,
 }
 
+const autoclickerInterval = {
+    level1Time: 10000,
+    level2Time: 9000,
+    level3Time: 8000,
+    level4Time: 7000,
+    level5Time: 6000,
+    level6Time: 5000,
+    level7Time: 4000,
+    level8Time: 3000,
+    level9Time: 2000,
+    level10Time: 1000,
+}
+
 let cookieMultiplier = multiplierCookies.level1;
-let clickPerSecond = autoclickerLevel.level0;
 let autoclickInterval;
+let labelOnAutoclickButton =  "1 x Hastighed";
 
 function UpdateCookieCounter() {
     document.getElementById("cookies").innerHTML = `${cookies} cookies`;
@@ -80,14 +66,14 @@ function ClickOnCookie() {
 }
 
 function AutoclickOnCookie(interval) {
+    UpdateCookieCounter();
     console.log("Interval: " + interval);
     if (autoclickInterval) {
         clearInterval(autoclickInterval);
     }
     
     function Autoclick() {
-        console.log("clickspersecond: " + clickPerSecond);
-        for (let i = 0; i < clickPerSecond; i++) {
+        for (let i = 0; i < 1; i++) {
             ClickOnCookie();
         }
     }
@@ -122,38 +108,34 @@ function MultiplierCookieStore() {
     UpdateCookieCounter();
 }
 
-
 function AutoclickerStore() {
     const autoclickerLevels = [
-        { level: autoclickerLevel.level0, price: autoclickerPrice.level1Price, time: autoclickerTime.level1Time, nextTime: autoclickerTime.level1, nextLevel: autoclickerLevel.level1, nextButtonText: "2 x Hastighed" },
-        { level: autoclickerLevel.level1, price: autoclickerPrice.level2Price, time: autoclickerTime.level2Time, nextTime: autoclickerTime.level2, nextLevel: autoclickerLevel.level2, nextButtonText: "3 x Hastighed" },
-        { level: autoclickerLevel.level2, price: autoclickerPrice.level3Price, time: autoclickerTime.level3Time, nextTime: autoclickerTime.level3, nextLevel: autoclickerLevel.level3, nextButtonText: "4 x Hastighed" },
-        { level: autoclickerLevel.level3, price: autoclickerPrice.level4Price, time: autoclickerTime.level4Time, nextTime: autoclickerTime.level4, nextLevel: autoclickerLevel.level4, nextButtonText: "5 x Hastighed" },
-        { level: autoclickerLevel.level4, price: autoclickerPrice.level5Price, time: autoclickerTime.level5Time, nextTime: autoclickerTime.level5, nextLevel: autoclickerLevel.level5, nextButtonText: "6 x Hastighed" },
-        { level: autoclickerLevel.level5, price: autoclickerPrice.level6Price, time: autoclickerTime.level6Time, nextTime: autoclickerTime.level6, nextLevel: autoclickerLevel.level6, nextButtonText: "7 x Hastighed" },
-        { level: autoclickerLevel.level6, price: autoclickerPrice.level7Price, time: autoclickerTime.level7Time, nextTime: autoclickerTime.level7, nextLevel: autoclickerLevel.level7, nextButtonText: "8 x Hastighed" },
-        { level: autoclickerLevel.level7, price: autoclickerPrice.level8Price, time: autoclickerTime.level8Time, nextTime: autoclickerTime.level8, nextLevel: autoclickerLevel.level8, nextButtonText: "9 x Hastighed" },
-        { level: autoclickerLevel.level8, price: autoclickerPrice.level9Price, time: autoclickerTime.level9Time, nextTime: autoclickerTime.level9, nextLevel: autoclickerLevel.level9, nextButtonText: "10 x Hastighed" },
-        { level: autoclickerLevel.level9, price: autoclickerPrice.level10Price, time: autoclickerTime.level10Time, nextTime: autoclickerTime.level10, nextLevel: autoclickerLevel.level10, nextButtonText: "Maxed" },
+        { buttonLabel: "1 x Hastighed", price: autoclickerPrice.level1Price, nextUprade: autoclickerInterval.level1Time, nextButtonLabel: "2 x Hastighed" },
+        { buttonLabel: "2 x Hastighed", price: autoclickerPrice.level2Price, nextUprade: autoclickerInterval.level2Time, nextButtonLabel: "3 x Hastighed" },
+        { buttonLabel: "3 x Hastighed", price: autoclickerPrice.level3Price, nextUprade: autoclickerInterval.level3Time, nextButtonLabel: "4 x Hastighed" },
+        { buttonLabel: "4 x Hastighed", price: autoclickerPrice.level4Price, nextUprade: autoclickerInterval.level4Time, nextButtonLabel: "5 x Hastighed" },
+        { buttonLabel: "5 x Hastighed", price: autoclickerPrice.level5Price, nextUprade: autoclickerInterval.level5Time, nextButtonLabel: "6 x Hastighed" },
+        { buttonLabel: "6 x Hastighed", price: autoclickerPrice.level6Price, nextUprade: autoclickerInterval.level6Time, nextButtonLabel: "7 x Hastighed" },
+        { buttonLabel: "7 x Hastighed", price: autoclickerPrice.level7Price, nextUprade: autoclickerInterval.level7Time, nextButtonLabel: "8 x Hastighed" },
+        { buttonLabel: "8 x Hastighed", price: autoclickerPrice.level8Price, nextUprade: autoclickerInterval.level8Time, nextButtonLabel: "9 x Hastighed" },
+        { buttonLabel: "9 x Hastighed", price: autoclickerPrice.level9Price, nextUprade: autoclickerInterval.level9Time, nextButtonLabel: "10 x Hastighed" },
+        { buttonLabel: "10 x Hastighed", price: autoclickerPrice.level10Price, nextUprade: autoclickerInterval.level10Time, nextButtonLabel: "Maxed" },
     ];
 
     for (let index in autoclickerLevels) {
-        console.log(1);
         const level = autoclickerLevels[index];
-        if (clickPerSecond === level.level) {
-            console.log(2);
+        if (labelOnAutoclickButton === level.buttonLabel) {
+            console.log("Next upgrade: " + level.nextButtonLabel);
+            console.log("Pris: " + level.price);
             if (cookies >= level.price) {
-                console.log(3);
                 cookies -= level.price;
-                document.getElementById("autoclicker").innerHTML = level.nextButtonText;
-                clickPerSecond = level.level;
-                console.log(level.level);
-                console.log(level.time);
-                AutoclickOnCookie(level.time);
+                console.log(level.nextButtonLabel);
+                labelOnAutoclickButton = level.nextButtonLabel;
+                document.getElementById("autoclicker").innerHTML = level.nextButtonLabel;
+                AutoclickOnCookie(level.nextUprade);
             }
             break;
         }
         continue;
     }
-    UpdateCookieCounter();
 }
