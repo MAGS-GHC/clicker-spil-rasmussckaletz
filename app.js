18b7f039956a01342c9380c72e955948030b34e7
rasmussckaletz <rasmus@leeloo.dk>
2023-10-11 11:12:35 +0200

    Update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Definer variabler til antal cookies og cookies i alt
 let cookies = 0;
 let cookiesInTotal = 0;
 
+// Definer de forskellige opgraderinger til cookies delen
 const multiplierCookies = {
     level1: 1,
     level2: 2,
@@ -26,6 +28,7 @@ const multiplierCookiePrice = {
     level10Price: 100000000,
 }
 
+// Tjekker hvilket level du er i og finder den tilsvarende opgraderingspris samt opdaterer knappens titel
 const multiplierLevels = [
     { level: multiplierCookies.level1, price: multiplierCookiePrice.level2Price, nextLevel: multiplierCookies.level2, nextButtonText: "4 x Cookies" },
     { level: multiplierCookies.level2, price: multiplierCookiePrice.level3Price, nextLevel: multiplierCookies.level3, nextButtonText: "8 x Cookies" },
@@ -38,6 +41,7 @@ const multiplierLevels = [
     { level: multiplierCookies.level9, price: multiplierCookiePrice.level10Price, nextLevel: multiplierCookies.level10, nextButtonText: "Maxed" }
 ];
 
+// Definer de forskellige opgraderinger til autoclicker delen
 const autoclickerPrice = {
     level1Price: 10,
     level2Price: 100,
@@ -64,55 +68,58 @@ const autoclickerInterval = {
     level10Time: 1000,
 }
 
+// Tjekker hvilken autoclicker knap titel, der vises og finder den tilsvarende opgraderingspris + interval samt opdaterer knappens titel
 const autoclickerLevels = [
-    { buttonLabel: "1 x Hastighed", price: autoclickerPrice.level1Price, nextUprade: autoclickerInterval.level1Time, nextButtonLabel: "2 x Hastighed" },
-    { buttonLabel: "2 x Hastighed", price: autoclickerPrice.level2Price, nextUprade: autoclickerInterval.level2Time, nextButtonLabel: "3 x Hastighed" },
-    { buttonLabel: "3 x Hastighed", price: autoclickerPrice.level3Price, nextUprade: autoclickerInterval.level3Time, nextButtonLabel: "4 x Hastighed" },
-    { buttonLabel: "4 x Hastighed", price: autoclickerPrice.level4Price, nextUprade: autoclickerInterval.level4Time, nextButtonLabel: "5 x Hastighed" },
-    { buttonLabel: "5 x Hastighed", price: autoclickerPrice.level5Price, nextUprade: autoclickerInterval.level5Time, nextButtonLabel: "6 x Hastighed" },
-    { buttonLabel: "6 x Hastighed", price: autoclickerPrice.level6Price, nextUprade: autoclickerInterval.level6Time, nextButtonLabel: "7 x Hastighed" },
-    { buttonLabel: "7 x Hastighed", price: autoclickerPrice.level7Price, nextUprade: autoclickerInterval.level7Time, nextButtonLabel: "8 x Hastighed" },
-    { buttonLabel: "8 x Hastighed", price: autoclickerPrice.level8Price, nextUprade: autoclickerInterval.level8Time, nextButtonLabel: "9 x Hastighed" },
-    { buttonLabel: "9 x Hastighed", price: autoclickerPrice.level9Price, nextUprade: autoclickerInterval.level9Time, nextButtonLabel: "10 x Hastighed" },
-    { buttonLabel: "10 x Hastighed", price: autoclickerPrice.level10Price, nextUprade: autoclickerInterval.level10Time, nextButtonLabel: "Maxed" },
+    { buttonLabel: "1 x Autoclicker", price: autoclickerPrice.level1Price, nextUprade: autoclickerInterval.level1Time, nextButtonLabel: "2 x Autoclicker" },
+    { buttonLabel: "2 x Autoclicker", price: autoclickerPrice.level2Price, nextUprade: autoclickerInterval.level2Time, nextButtonLabel: "3 x Autoclicker" },
+    { buttonLabel: "3 x Autoclicker", price: autoclickerPrice.level3Price, nextUprade: autoclickerInterval.level3Time, nextButtonLabel: "4 x Autoclicker" },
+    { buttonLabel: "4 x Autoclicker", price: autoclickerPrice.level4Price, nextUprade: autoclickerInterval.level4Time, nextButtonLabel: "5 x Autoclicker" },
+    { buttonLabel: "5 x Autoclicker", price: autoclickerPrice.level5Price, nextUprade: autoclickerInterval.level5Time, nextButtonLabel: "6 x Autoclicker" },
+    { buttonLabel: "6 x Autoclicker", price: autoclickerPrice.level6Price, nextUprade: autoclickerInterval.level6Time, nextButtonLabel: "7 x Autoclicker" },
+    { buttonLabel: "7 x Autoclicker", price: autoclickerPrice.level7Price, nextUprade: autoclickerInterval.level7Time, nextButtonLabel: "8 x Autoclicker" },
+    { buttonLabel: "8 x Autoclicker", price: autoclickerPrice.level8Price, nextUprade: autoclickerInterval.level8Time, nextButtonLabel: "9 x Autoclicker" },
+    { buttonLabel: "9 x Autoclicker", price: autoclickerPrice.level9Price, nextUprade: autoclickerInterval.level9Time, nextButtonLabel: "10 x Autoclicker" },
+    { buttonLabel: "10 x Autoclicker", price: autoclickerPrice.level10Price, nextUprade: autoclickerInterval.level10Time, nextButtonLabel: "Maxed" },
 ];
 
-let cookieMultiplier = multiplierCookies.level1;
+let cookieMultiplier = multiplierCookies.level1; // Definer at cookieMultiplier skal starte i level 1
 let autoclickInterval;
-let labelOnAutoclickButton =  "1 x Hastighed";
+let labelOnAutoclickButton =  "1 x Autoclicker"; // Definer første knaps titel til autoclicker
 
+// Generel opdatering af selve knapperne og værdier samt design
 function UpdateCookieCounter() {
     let cookieKnapHTML = document.getElementById("cookieknap");
     let autoclickerKnapHTML = document.getElementById("autoclicker");
     document.getElementById("cookies").innerHTML = `${cookies} cookies`;
     document.getElementById("cookiesInTotal").innerHTML = `${cookiesInTotal} cookies in total`;
     document.title = `${cookies} cookies - Clicker Spil`;
-    cookieKnapHTML.disabled = true;
-    autoclickerKnapHTML.disabled = true;
+    cookieKnapHTML.disabled = true; // Knappen kan ikke klikkes på samt er gennemsigtig, hvis den ikke kan bruges
+    autoclickerKnapHTML.disabled = true; // Knappen kan ikke klikkes på samt er gennemsigtig, hvis den ikke kan bruges
     for (let index in multiplierLevels) {
         const level = multiplierLevels[index];
         if (cookieMultiplier === level.level && cookies >= level.price) {
-            cookieKnapHTML.disabled = false;
+            cookieKnapHTML.disabled = false; // Hvis if statement returnerer true værdi, bliver knappen brugbar og synlig
         }
     }
-
+    
     for (let index in autoclickerLevels) {
         const level = autoclickerLevels[index];
         if (labelOnAutoclickButton === level.buttonLabel && cookies >= level.price) {
-            autoclickerKnapHTML.disabled = false;
+            autoclickerKnapHTML.disabled = false; // Hvis if statement returnerer true værdi, bliver knappen brugbar og synlig
         }
     }
 }
 
+// Definer hvad der sker, når der klikkes på cookie
 function ClickOnCookie() {
-    cookies += cookieMultiplier;
-    cookiesInTotal += cookieMultiplier;
+    cookies += cookieMultiplier; // tilføj antal cookies til tæller (cookieMultiplier angiver det level og antal cookies, vi har købt)
+    cookiesInTotal += cookieMultiplier; // tilføj antal cookies til total tæller (cookieMultiplier angiver det level og antal cookies, vi har købt)
     UpdateCookieCounter();
 }
 
+// Definer autoclickerens function efter vi har købt autoclickeren første gang
 function AutoclickOnCookie(interval) {
     UpdateCookieCounter();
-    console.log("Interval: " + interval);
     if (autoclickInterval) {
         clearInterval(autoclickInterval);
     }
@@ -125,15 +132,17 @@ function AutoclickOnCookie(interval) {
     autoclickInterval = setInterval(Autoclick, interval);
 }
 
+// Function, hvor vi køber opgraderinger til vores multiplier
 function MultiplierCookieStore() {
     for (let index in multiplierLevels) {
         const level = multiplierLevels[index];
         if (cookieMultiplier === level.level) {
-            if (cookies >= level.price) {
-                cookies -= level.price;
+            if (cookies >= level.price) { // Tjekker om vi har cookies nok til opgradering
+                cookies -= level.price; // Fratrækker opgraderingspris fra cookies
                 document.getElementById("cookieknap").innerHTML = level.nextButtonText;
-                cookieMultiplier = level.nextLevel;
-                UpdateCookieCounter(level.price);
+                cookieMultiplier = level.nextLevel; // Fastsætter nyt level
+                UpdateCookieCounter(level.price); // Opdaterer counter
+                window.alert("Du opgraderede antal cookies pr. klik!"); // Giver achievement / confirmation popup ved køb
             }
             break;
         }
@@ -141,18 +150,17 @@ function MultiplierCookieStore() {
     }
 }
 
+// Function, hvor vi køber opgraderinger til vores autoclicker
 function AutoclickerStore() {
     for (let index in autoclickerLevels) {
         const level = autoclickerLevels[index];
         if (labelOnAutoclickButton === level.buttonLabel) {
-            console.log("Next upgrade: " + level.nextButtonLabel);
-            console.log("Pris: " + level.price);
-            if (cookies >= level.price) {
-                cookies -= level.price;
-                console.log(level.nextButtonLabel);
+            if (cookies >= level.price) { // Tjekker om vi har cookies nok til opgradering
+                cookies -= level.price; // Fratrækker opgraderingspris fra cookies
                 labelOnAutoclickButton = level.nextButtonLabel;
                 document.getElementById("autoclicker").innerHTML = level.nextButtonLabel;
-                AutoclickOnCookie(level.nextUprade, level.price);
+                AutoclickOnCookie(level.nextUprade, level.price); // Fastsætter nyt level
+                window.alert("Du opgraderede din autoclicker!"); // Giver achievement / confirmation popup ved køb
             }
             break;
         }
